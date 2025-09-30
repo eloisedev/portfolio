3f4fbad1488be3d5a574bd371ab08a2b29cad5a2
eloise devito <eloisedevito@icloud.com>
2025-09-30 09:17:18 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+//JAVASCRIPT FOR ELOISE DEVITO PORTFOLIO
+//coded by Eloise DeVito 
+//last updated: 9/30/25
+
+const idCard = document.getElementById("id");
+const postitCard = document.getElementById("postit");
+const poloroidCard = document.getElementById("poloroid");
+const braceletCard = document.getElementById("bracelet");
+
+idCard.onclick = function () {
+    //TBA: link to lower in the page: about me
+};
+
+postitCard.onclick = function () {
+    //TBA: link to write email
+};
+
+poloroidCard.onclick = function () {
+    //TBA: link to recent project
+};
+
+braceletCard.onclick = function () {
+    //TBA: link to lower in the page: skills/knowledge
+};
